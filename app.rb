1dabd4c36460e96c7044bf07f73eed225ba9d097
ssstroud23 <ssstroud23@gmail.com>
2018-07-17 18:26:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
+<<<<<<< HEAD
     "Started my server using Shotgun!"
+=======
+    "Welcome to your app! I built this! Started my server using shotgun"
+>>>>>>> e4955592dc30732ba80cfdca6d12919b94874f47
   end
 
 end
